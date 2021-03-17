9e03aecb64ca538c3be2d53f1ad6b4ff584a95da
Matt Chapman <matt@mattchapman.io>
2021-03-17 18:16:14 +1300

    Create initial environment

---
+++ src/test/index.test.ts
@@ -0,0 +1,11 @@
+import chai from "chai";
+
+const { expect } = chai;
+
+describe("Example test case", function () {
+  it("should calculate 2 + 2", () => {
+    const result = 2 + 2;
+
+    expect(result).to.equal(4);
+  });
+});
